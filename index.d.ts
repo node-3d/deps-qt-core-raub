701d3b0c344ed9b2649bd41017e83330f89acfb7
Luis Blanco <luisblanco1337@gmail.com>
2026-06-28 17:32:48 +0400

    Update Qt Core dependency package tooling

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,9 @@
-declare module "deps-qt-core-raub" {
-	/**
-	 * Path to binaries
-	 *
-	 * Platform binary directory absolute path
-	*/
-	export const bin: string;
-	/**
-	 * Path to includes
-	 *
-	 * Include directory for this module
-	*/
-	export const include: string;
-}
+import type { getPaths } from '@node-3d/addon-tools';
+
+type AddonPaths = ReturnType<typeof getPaths>;
+
+export declare const bin: AddonPaths['bin'];
+export declare const include: AddonPaths['include'];
+
+declare const paths: AddonPaths;
+export default paths;
